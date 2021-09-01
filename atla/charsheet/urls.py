8278b10257fdbe7559dc6ff99787d7a15adbd8ca
Ryan Buchmeier <rwbleo@gmail.com>
2021-09-01 06:02:24 -0600

    Minor qol adjustments - admin, charsheet index

--- atla/charsheet/urls.py
+++ atla/charsheet/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.charsheetList, name='characters-index'),
     path('<int:character_id>', views.charsheet, name='charsheet'),
     path('my-characters', views.charsheetList, name='my-characters'),
     path('new-character', views.newCharacter, name='new-character'),
